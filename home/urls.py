b069943a119cad11404577db475116a460f1caa2
ajandris <andris.jancevskis@gmail.com>
2025-10-03 03:01:56 +0100

    ADD template, logo and allauth login

--- home/urls.py
+++ home/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('', views.index, name='index'),
 ]
