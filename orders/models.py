68bd1e4a8eb2780adae1d938298bb265af54fb90
ajandris <andris.jancevskis@gmail.com>
2026-01-08 06:17:01 +0000

    UPDATE test order functions and FIX logical double columns in Order model.

--- orders/models.py
+++ orders/models.py
@@ -22,16 +22,12 @@ class Order(models.Model):
     order_no = models.CharField(max_length=50, unique=True)
     owner = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='order_owner')
     order_date = models.DateTimeField(auto_now_add=True)
-    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, null=False, blank=False,
-                                   verbose_name='Discount applied')
     subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, null=False, blank=False,
                                    verbose_name='Subtotal Calculated')
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, null=False, blank=False,
                                 verbose_name='Order Total Calculated')
     vat_included = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, null=False, blank=False,
                                        verbose_name='Included VAT Calculated')
-    shipping_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, null=False, blank=False,
-                                          verbose_name='shipping price applied')
     currency = models.CharField(max_length=3, default='GBP', verbose_name='Order Currency', null=False,
                                 blank=False)
     cart_no = models.CharField(max_length=100, verbose_name='Cart Number', null=False, blank=False)
@@ -48,16 +44,15 @@ class Order(models.Model):
 
     inserted_at = models.DateTimeField(auto_now_add=True, verbose_name='DateTime when record inserted')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='DateTime when record updated')
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='order_user_records')
 
     class Meta:
         db_table = 'orders'
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
 
-    @property
     def __str__(self):
-        return self.order_no
+        return f"Order {self.order_no}"
 
 
 class OrderItem(models.Model):
@@ -71,7 +66,6 @@ class OrderItem(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='DateTime when record updated')
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
-    @property
     def __str__(self):
         return self.sku
 
---
+++ test/02_orders/test_order.py
@@ -0,0 +1,10 @@
+import pytest
+from decimal import Decimal
+
+
+def test_create_order():
+    pass
+
+
+def test_validate_order():
+    pass
