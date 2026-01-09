0a943bd79f2745322ded1cf63581393d14b5be93
ajandris <andris.jancevskis@gmail.com>
2026-01-09 21:56:39 +0000

    FIX cart number

--- orders/views.py
+++ orders/views.py
@@ -7,7 +7,7 @@ def process_payment(request):
     """
     Process payment from checkout
     """
-    cart_no = request.session.get('cart_no')
+    cart_no = request.session.get('cart_number')
     cart = None
     if cart_no:
         cart = Cart.objects.get(cart_number=cart_no)
@@ -24,6 +24,7 @@ def process_payment(request):
     # check/ validate cart itself against user changes
     totals = calculate_order(cart_no)
     checkout_hash = request.POST.get('checkout_hash')
+
     if checkout_hash != totals['cart_hash']:
         messages.error(request, "Your cart has been changed. Please review your cart before payment.")
         return redirect('cart')
